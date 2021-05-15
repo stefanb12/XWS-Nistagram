e6346c9cb5cfc9e38a4a6906552717f1693b202b
matija-mijalkovic <matija.mijalkovic@gmail.com>
2021-05-15 18:57:13 +0200

    Profile microservice model created

---
+++ nistagram-backend/Nistagram/ProfileMicroservice/Model/ProfileVerificationRequest.cs
@@ -0,0 +1,23 @@
+﻿using ProfileMicroservice.Model.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProfileMicroservice.Model
+{
+    public class ProfileVerificationRequest
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public UserCategory Category { get; set; }
+        public string DocumentPicture { get; set; }
+        public bool Accepted { get; set; }
+        public bool Processed { get; set; }
+
+        public ProfileVerificationRequest()
+        {
+        }
+    }
+}
---
+++ nistagram-backend/Nistagram/ProfileMicroservice/Model/User.cs
@@ -0,0 +1,23 @@
+﻿using ProfileMicroservice.Model.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProfileMicroservice.Model
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string MobilePhone { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public Gender Gender { get; set; }
+
+        public User()
+        {
+        }
+    }
+}
