9d543e0b4d0e455e882d54c54112d67d227e0739
aleksaivanic <ivanicaleksa98@gmail.com>
2021-06-16 01:09:47 +0200

    Profile privacy settings frontend implemented

---
+++ nistagram-frontend/src/components/profile-settings/VerifyProfile.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class VerifyProfile extends Component {
+  state = {};
+  render() {
+    return (
+      <div
+        className="card"
+        style={{ float: "right", marginRight: "20%", width: "50%" }}
+      >
+        <div className="card-header">
+          <h5 className="card-title">Verify Profile</h5>
+        </div>
+        <div className="card-body">
+          
+        </div>
+      </div>
+    );
+  }
+}
+
+export default VerifyProfile;
