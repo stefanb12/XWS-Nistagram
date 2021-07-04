785eca1a13e4664e953f118af7a564e0c591ed06
Stefan <stefan.savic98@gmail.com>
2021-07-04 21:58:34 +0200

    Registration implemented

---
+++ agent-app-backend/WebShop/WebShop/Dto/RegistrationDto.cs
@@ -0,0 +1,17 @@
+﻿using WebShop.Model.Enum;
+
+namespace WebShop.Dto
+{
+    public class RegistrationDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public UserRole UserRole { get; set; }
+
+        public RegistrationDto()
+        {
+        }
+    }
+}
